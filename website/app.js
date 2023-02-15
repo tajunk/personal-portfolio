362e6111f66862481f08c7af43c6bb9bb3afc59a
Taylor <35233492+tajunk@users.noreply.github.com>
2023-02-14 22:26:54 -0700

    small fix2
    typos, css

--- website/app.js
+++ website/app.js
@@ -110,7 +110,7 @@ const codeProject = [
     id: 1,
     projTitle: "React / .NET Web App",
     projText:
-      "Full stack web application created with a React based front end that serves C# code on a .NET framework back end with a MSQL Server database. Simple application that serves as a forum or bulletin board that multiple users can access and view, create and answer questions posed by themselves and/or others. Some functionality is only available when logged in, authentication is handled with a service called Auth0, and data is updated dynamically as it is added and stored live. Hosting is down as of 01/2023, likely will rebuild on something other than Azure.",
+      "Full stack web application created with a React based front end that serves C# code on a .NET framework back end with a MSQL Server database. Simple application that serves as a forum or bulletin board that multiple users can access and view, create and answer questions posed by themselves and/or others. Some functionality is only available when logged in, authentication is handled with a service called Auth0, and data is updated dynamically as it is added and stored live. Hosting currently down",
     projLink: "",
     projVideo: "/videos/qareact-project.mp4",
     projIcon1: "icons/react.svg",
@@ -132,7 +132,7 @@ const codeProject = [
     id: 3,
     projTitle: "Javascript / HTML Browser Game",
     projText:
-      "Small proof of concept game developed using just Javascript code with a HTML5 framework called Phaser. Most concepts with this project are very Phaser specific but presented a fun alternative to typical useages with Javascipt. The game itself is a typical 'endless' jump type game where carrots and platforms will continue to generate above the character as they jump and the goal is to get a high score without falling off the screen or a game over will be presented and the user is prompted to restart. ",
+      "Small proof of concept game developed using just Javascript code with an HTML5 framework called Phaser. Most concepts used within this project are very Phaser specific but presented a fun alternative to typical useages with Javascipt. The game itself is a typical 'endless' jump style of game where carrots and platforms will continue to generate above the character as they jump, the goal is to get a high score without falling off the screen. If that happens, a game over will be presented and the user is prompted to restart. ",
     projLink: "https://infinite-jumping-game.netlify.app/",
     projVideo: "/videos/jsgame-project.mp4",
     projIcon1: "icons/javascript.svg",
@@ -146,7 +146,7 @@ const webProject = [
     id: 1,
     projTitle: "Salon Boutique WordPress Website",
     projText:
-      "Proof of concept WordPress website not attached to any real world business but done for experimentation and learning purposes. Implementing certain business needs, like a scheduling system in the case of this project, are made simple when compared to creating a project from scratch.",
+      "Proof of concept WordPress website built as a portfolio piece to experiment and for learning purposes. Implementing certain business needs, like a scheduling system in the case of this project, were made simpler with the use of WordPress when compared to creating a project from scratch.",
     projLink: "http://taylora15.sg-host.com/",
     projVideo: "/videos/salon-project.mp4",
     projIcon1: "icons/wordpress.svg",
@@ -157,7 +157,7 @@ const webProject = [
     id: 2,
     projTitle: "Industrial Sales WordPress Website",
     projText:
-      "Proof of concept WordPress website not attached to any real world business but done for experimentation and learning purposes. Implementing certain business needs, like inventory and shop in the case of this project, are made simple when compared to creating a project from scratch.",
+      "Proof of concept WordPress website built as a portfolio piece to experiment and for learning purposes. Implementing certain business needs, like inventory and shop in the case of this project, were made simpler with the use of WordPress when compared to creating a project from scratch.",
     projLink: "http://taylora16.sg-host.com/",
     projVideo: "/videos/industry-project.mp4",
     projIcon1: "icons/wordpress.svg",
@@ -168,7 +168,7 @@ const webProject = [
     id: 3,
     projTitle: "Portfolio Custom Javascript Website",
     projText:
-      "This website! The video shows an old version of the site, this portfolio has served as a fun project to revisit as I learn new things or look to experiment over time. The site itself is created from scratch and uses just HTML, 800 plus lines of CSS and Javascript code to achieve all the functionality. A few noteables that this includes are the dynamic scrolling (after calculating screen position) from the NAV, the fixed NAV based on screen position, and querying for each of my projects from their objects then dynamically displaying them in the HTML with a function based on a selection.",
+      "My personal portfolio! The video shows an old version of the site, this portfolio has served as a fun project to revisit as I learn new things or look to experiment over time. The site itself is created from scratch and uses just HTML, 800 plus lines of CSS (*) and Javascript code to achieve all the functionality. A few noteable functionalities on this site are the dynamic scrolling (after calculating screen position) from the NAV, the fixed NAV based on screen position, and querying for each of my projects from their objects then dynamically displaying them in the HTML with a function based on a selection.   *(CSS could be cleaner for sure!)*",
     projLink: "",
     projVideo: "/videos/portfolio-project.mp4",
     projIcon1: "icons/javascript.svg",
